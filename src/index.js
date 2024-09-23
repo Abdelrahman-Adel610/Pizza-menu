34f6a2e4b99465984eb4f8fd26a65dd01ee58a6c
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-09-23 12:06:40 +0300

    ➕ Added starter files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
